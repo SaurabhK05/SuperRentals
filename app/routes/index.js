72f7e4f9ba8ac26d992cc15335e7a05907607e96
SaurabhK05 <saurabh.k150600@gmail.com>
2021-12-27 17:48:48 +0530

    New Updated Code

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,25 +1,27 @@
 import Route from '@ember/routing/route';
 // import fetch from 'fetch';
+const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
+export default class IndexRoute extends Route {
+    async model() {
+        // console.log(this.store.get(rental.json));
 
-export default class IndexRoute extends Route{
-    async model(){
-        // let response = await fetch('/api/rentals.json');
-        // let parsed = await response.json();
-        // return parsed;
-        return{
-            title: 'Grand Old Mansion',
-            owner: 'Veruca Salt',
-            city: 'San Francisco',
-            location: {
-                lat: 37.7749,
-                lng: -122.4194,
-            },
-            id: 'grand-old-mansion',
-            category: 'Estate',
-            type: 'Stanalone',
-            bedrooms: 15,
-            image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
-            description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.',
-        };
+        // const response1 = await fetch('/api/rentals.json').then(results => {return resul);
+        let response = await fetch('/api/rentals.json');
+
+        let { data } = await response.json();
+
+        return data.map((model) => {
+            let { attributes } = model;
+            let type;
+
+            if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
+                type = 'Community';
+            } else {
+                type = 'Standalone';
+            }
+
+            return { type, ...attributes };
+        }
+        );
     }
 }
